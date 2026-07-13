32b059eca9dcbd68123f34b4a3352481c497a87d
Fede Castiglione <feder102@gmail.com>
2026-07-13 09:58:42 -0300

    build: increment app version code to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.aistudio.saludfamiliar.kxmpzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
